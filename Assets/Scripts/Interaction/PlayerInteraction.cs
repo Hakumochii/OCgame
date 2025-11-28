1beb4736621a3970d77afc56930cdd88d421c526
Haku <stacia1402@gmail.com>
2025-11-28 20:20:24 +0100

    start på interaction

---
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    void OnCollisionEnter(collider other)
+    {
+        if (other.tag == "enterance")
+        {
+            
+        }
+    }
+
+}
